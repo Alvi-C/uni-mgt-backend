9ffd4ed2168dcde2d3df2a70236a9bf7e0551cd2
Alvi Chowdhury <alvic5050@gmail.com>
2024-05-25 22:52:44 +0600

    [Updated] routes > index.t, write the code in new way

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -11,8 +11,16 @@ router.get('/', (req: Request, res: Response) => {
   });
 });
 
-// student route
-router.use('/api/v1/student', studentRouter);
-router.use('/api/v1/users', userRouter);
+// router.use('/api/v1/student', studentRouter);
+// router.use('/api/v1/users', userRouter);
+
+const moduleRoutes = [
+  { path: '/api/v1/users', route: userRouter },
+  { path: '/api/v1/student', route: studentRouter },
+];
+
+moduleRoutes.forEach(route => {
+  router.use(route.path, route.route);
+});
 
 export default router;
